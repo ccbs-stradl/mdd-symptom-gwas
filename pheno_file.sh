0aa1670bf5d8bd7ed04d352e038701e317579218
Bradley Jermy <39912643+Bjermy@users.noreply.github.com>
2019-07-09 18:11:00 +0100

    Update pheno_file.sh
    Added code to extract Rotterdam and GSK separately to overcome the issue of duplicated IDs. Next step is to remove overlap with UKB then the pheno file is ready for the RICOPILI pipeline

--- pheno_file.sh
+++ pheno_file.sh
@@ -22,6 +22,10 @@ R
 ###########################################################################################################################
 
 library(yaml)
+library(gdata)
+library(stringr)
+library(plyr)
+library(dplyr)
 
 config <- yaml.load_file('config.yaml')
 
@@ -46,38 +50,58 @@ for (i in symptoms) {
 selectpheno(variable=i, workingdirectory=file.path(config$data$pgc$mdd$v1, "secondary_phenotypes/1216_update"), outputdirectory=config$data$pgc$mdd$output)
 }
 
-#Part 2
-##Subset by study ID that is relevant to my cohorts - cases and controls
+#Rotterdam dataset to be extracted separately
+rotterdam <- read.xls(file.path(config$data$pgc$mdd$output, "secondary_phenotypes/1216_update/ROTTERDAM_PGC2updated.xls", header=T)
+
+#GSK dataset to be extracted separately
+gsk <- read.xls(file.path(config$data$pgc$mdd$output, "secondary_phenotypes/1216_update/GSK_PGC2updated.xls", header=T)
+
+
+#Save down these separately extracted files
 symptoms <- c("MDD1", "MDD2", "MDD3a", "MDD3b", "MDD4a", "MDD4b", "MDD5a", "MDD5b", "MDD6", "MDD7", "MDD8", "MDD9")
 for (i in symptoms) {
-MDD <-read.table(file.path(config$data$pgc$mdd$output, paste0(i,"_PGCMDD2.tsv")), sep="\t", header=T)
+rot <- rotterdam[,c('ID1','ID2','Study','Sub.study',i)]
+write.table(rot,paste(i,"rotterdam_PGC2.tsv",sep="_"),sep="\t")
+gs <- gsk[,c('ID1','ID2','Study','Sub.study',i)]
+write.table(gs,paste(i,"gsk_PGC2.tsv",sep="_"),sep="\t")
+}
+
+#Part 2
+##Subset by study ID that is relevant to my cohorts - cases and controls
+for (i in symptoms) {
+rotterdam1 <- read.table(file.path(config$data$pgc$mdd$output, paste0(i,'_rotterdam_PGC2.tsv', sep="")), sep="\t", header=T)
+gsk1 <- read.table(file.path(config$data$pgc$mdd$output,paste0(i,'_gsk_PGC2.tsv', sep="")), sep="\t", header=T)
+mdd1 <-read.table(file.path(config$data$pgc$mdd$output, paste0('symptom_',i,'_PGCMDD2.xls', sep="")), header=T)
+mdd1_norotgsk <- mdd1 %>% 
+  				filter(!str_detect(ID1, '_rot4_eur_sr|_gsk2_eur_sr_'))
+MDD <- rbind.fill(mdd1_norotgsk, rotterdam1, gsk1)
+
 keepStudy <- c("COF", "Colaus", "NESDA/NTR", "ROTTERDAM", "SHIP-0", "RS")
 #Change pheno file for 1=controls, 2=cases and -9=unknowns as stated on RICOPILI
 MDD[,i][!MDD$Study %in% keepStudy] <- -9 #Remove cases and controls not in the correct cohort
 MDD[,i][is.na(MDD[,i])] <- -9  #Recode NAs to -9
 MDD[,i][MDD[,i]==1] <- 2 
 MDD[,i][MDD[,i]==0] <- 1 
-write.csv(MDD, file.path(config$data$pgc$mdd$output, paste0("symptom_",i,"_PGCMDD2_recoded.csv")), row.names=F)
+print(table(MDD[[i]]))
+write.table(MDD, file.path(config$data$pgc$mdd$output, paste0("symptom_",i,"_PGCMDD2_recoded.tsv", sep="")), sep="\t", row.names=F)
 }
+
 
 #Check coding of phenos to see if any adjustments are required
 for (i in symptoms) {
-MDD <-read.csv(file.path(config$data$pgc$mdd$output, paste0("symptom_",i,"_PGCMDD2_recoded.csv")), header=T)
+MDD <-read.table(file.path(config$data$pgc$mdd$output, paste0("symptom_",i,"_PGCMDD2_recoded.tsv", sep="")), sep="\t" header=T)
 print(dim(MDD))
 print(table(MDD[[i]]))
-} #Everything seems to be working
+} #Everything seems to be working - 6th dataset needs a 9 recoded as -9
 
 #Part 3
-#Remove overlapping samples with UKB
-symptoms <- c("MDD1", "MDD2", "MDD3a", "MDD3b", "MDD4a", "MDD4b", "MDD5a", "MDD5b", "MDD6", "MDD7", "MDD8", "MDD9")
-UKB <- read.table(config$data$pgc$mdd$ukb_remove, sep="\t", header=F)
-for (i in symptoms) {
-MDD <-read.csv(file.path(config$data$pgc$mdd$output, paste0("symptom_",i,"_PGCMDD2_recoded.csv")), header=T)
-print(table(MDD[[i]]))
+#Remove overlapping samples with UKB - remove according to ID1 and ID2 - UP TO HERE!
+#symptoms <- c("MDD1", "MDD2", "MDD3a", "MDD3b", "MDD4a", "MDD4b", "MDD5a", "MDD5b", "MDD6", "MDD7", "MDD8", "MDD9")
+#UKB <- read.table(config$data$pgc$mdd$ukb_remove, sep="\t", header=F)
+#for (i in symptoms) {
+#MDD <-read.csv(file.path(config$data$pgc$mdd$output, paste0("symptom_",i,"_PGCMDD2_recoded.csv")), header=T)
+#print(table(MDD[[i]]))
 #MDD <- MDD[!MDD$ID1 %in% UKB$V2]
-print(sum(MDD$ID2 %in% UKB$V3)) #Matches with 315
-print(sum(MDD$ID1 %in% UKB$V2)) #Matches with 2287 people instead of 959 - duplicates in my dataset?
-print(sum(duplicated(MDD$ID1)))  #duplicates in the datasets - ask Mark about this. 
-#write.csv(MDD, paste("symptom_",i,"_PGCMDD2_recoded.csv", sep=""))
+
 }
 
