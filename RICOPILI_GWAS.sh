b62166388caff324f225ea26242d98c4ceae1951
Bradley Jermy <39912643+Bjermy@users.noreply.github.com>
2019-07-26 09:17:12 +0100

    Case/Control Cohort Symptom level GWAS

--- RICOPILI_GWAS.sh
+++ RICOPILI_GWAS.sh
@@ -7,23 +7,32 @@ tar -xvzf rp_bin.2019_Jun_25.001.tar.gz
 
 #Create custom installation file in ricopili
 
-
 #Softlink to testing datasets 
 ln -s //home/pgcdac/DWFV2CJb8Piv_0116_pgc_data/mdd/wave2/v1/* /home/jermy/
-
+ln -s //home/pgcdac/DWFV2CJb8Piv_0116_pgc_data/mdd/wave2/v1_shp/* /home/jermy/ 
 
 #TEST!!!  Postimp RICOPILI code - run this from my home directory now all softlinks have been set-up
 postimp_navi --out MDD1_case_con --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --refiex refiex.mddw2v01
 
-#Create datasets_info with information on the files you are interested in - not including SHIP0 yet as waiting on access. 
+#Create datasets_info with information on the files you are interested in 
 touch datasets_info
 vim datasets_info
-dasuqc1_mdd_cof3_eur_sr-qc.hg19.ch.fl
-dasuqc1_mdd_col3_eur_sr-qc.hg19.ch.fl
-dasuqc1_mdd_rot4_eur_sr-qc.hg19.ch.fl
-dasuqc1_mdd_nes1_eur_sr-qc.hg19.ch.fl
+mdd_cof3_eur_sr-qc.hg19.ch.fl
+mdd_col3_eur_sr-qc.hg19.ch.fl
+mdd_rot4_eur_sr-qc.hg19.ch.fl
+mdd_nes1_eur_sr-qc2.hg19.ch.fl
+mdd_shp0_eur_sr-qc.hg19.ch.fl
 
-#Actual code
-postimp_navi --out MDD1_noship --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --bgscore --pheno MDD1_PGCMDD2_final.txt
-
- 
+#Run analysis
+postimp_navi --out MDD1_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD1_PGCMDD2_final.txt #Done
+postimp_navi --out MDD2_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD2_PGCMDD2_final.txt #Done
+postimp_navi --out MDD3a_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD3a_PGCMDD2_final.txt #Done
+postimp_navi --out MDD3b_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD3b_PGCMDD2_final.txt
+postimp_navi --out MDD4a_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD4a_PGCMDD2_final.txt
+postimp_navi --out MDD4b_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD4b_PGCMDD2_final.txt
+postimp_navi --out MDD5a_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD5a_PGCMDD2_final.txt
+postimp_navi --out MDD5b_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD5b_PGCMDD2_final.txt
+postimp_navi --out MDD6_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD6_PGCMDD2_final.txt
+postimp_navi --out MDD7_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD7_PGCMDD2_final.txt
+postimp_navi --out MDD8_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD8_PGCMDD2_final.txt
+postimp_navi --out MDD0_CaseControl --mds MDD29.0515.nproj.menv.mds_cov --coco 1,2,3,4,5,6 --popname eur --pheno MDD9_PGCMDD2_final.txt
