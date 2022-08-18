62de46bb4047a7bde1f46fa6957e034576c67c26
Mark James Adams <mark.adams@ed.ac.uk>
2022-08-18 14:47:13 +0100

    calculate PRS for all samples CFA gwas

--- prs/genscot_prs_do.sh
+++ prs/genscot_prs_do.sh
@@ -9,3 +9,9 @@ qsub genscot_prs_all.sh ../meta/usergwas/agds_pgc.sui.sumstats.txt.gz clinsui
 qsub genscot_prs_all.sh ../meta/usergwas/agds_pgc.appinc.sumstats.txt.gz clinappinc
 
 qsub genscot_prs_all.sh ../meta/usergwas/agds_pgc.appdec.sumstats.txt.gz clinappdec
+
+qsub genscot_prs_all.sh ../meta/usergwas/all.common.sumstats.txt.gz common
+
+qsub genscot_prs_all.sh ../meta/usergwas/all.affect.sumstats.txt.gz affect
+
+qsub genscot_prs_all.sh ../meta/usergwas/all.neuroveg.sumstats.txt.gz neuroveg
